8044ca1f71254130c9645eeb164764247921e1b0
Vonne Benedict Gelaga <89884729+VonneGelaga@users.noreply.github.com>
2024-05-21 20:24:08 +0800

    Updated login.cy.js

--- client/cypress/e2e/login.cy.js
+++ client/cypress/e2e/login.cy.js
@@ -3,49 +3,49 @@ describe('login page', () => {
     cy.visit('http://localhost:5173/login');
   });
 
-  // it("should show validation errors when leaving all fields blank", () => {
-  //   cy.get('[type="submit"]').click();
-  //   cy.get("input#username:invalid").should("have.length", 1);
-  //   cy.get("#username").then(($input) => {
-  //     expect($input[0].validationMessage).to.eq("Please fill out this field.");
-  //   });
-  // });
+  it("should show validation errors when leaving all fields blank", () => {
+    cy.get('[type="submit"]').click();
+    cy.get("input#username:invalid").should("have.length", 1);
+    cy.get("#username").then(($input) => {
+      expect($input[0].validationMessage).to.eq("Please fill out this field.");
+    });
+  });
 
-  // it("should show a validation error when leaving only the 'Username' field blank", () => {
-  //   cy.get("#password").type('kimocheese1234');
-  //   cy.get('[type="submit"]').click();
-  //   cy.get("input#username:invalid").should("have.length", 1);
-  //   cy.get("#username").then(($input) => {
-  //     expect($input[0].validationMessage).to.eq("Please fill out this field.");
-  //   });
-  // });
+  it("should show a validation error when leaving only the 'Username' field blank", () => {
+    cy.get("#password").type('kimocheese1234');
+    cy.get('[type="submit"]').click();
+    cy.get("input#username:invalid").should("have.length", 1);
+    cy.get("#username").then(($input) => {
+      expect($input[0].validationMessage).to.eq("Please fill out this field.");
+    });
+  });
 
-  // it("should show a validation error when leaving only the 'Password' field blank", () => {
-  //   cy.get("#username").type('kimocheese');
-  //   cy.get('[type="submit"]').click();
-  //   cy.get("input#password:invalid").should("have.length", 1);
-  //   cy.get("#password").then(($input) => {
-  //     expect($input[0].validationMessage).to.eq("Please fill out this field.");
-  //   });
-  // });
+  it("should show a validation error when leaving only the 'Password' field blank", () => {
+    cy.get("#username").type('kimocheese');
+    cy.get('[type="submit"]').click();
+    cy.get("input#password:invalid").should("have.length", 1);
+    cy.get("#password").then(($input) => {
+      expect($input[0].validationMessage).to.eq("Please fill out this field.");
+    });
+  });
 
-  // it("should show an error message when the username does not exist", () => {
-  //   cy.get("#username").type('test');
-  //   cy.get("#password").type('1qaz2wsx');
-  //   cy.get('[type="submit"]').click();
+  it("should show an error message when the username does not exist", () => {
+    cy.get("#username").type('test');
+    cy.get("#password").type('1qaz2wsx');
+    cy.get('[type="submit"]').click();
 
-  //   cy.wait('@loginRequest');
+    cy.wait('@loginRequest');
 
-  //   cy.get('.error').should('have.text', 'username not found');
-  // });
+    cy.get('.error').should('have.text', 'username not found');
+  });
 
-  // it("should show an error message when the password is incorrect", () => {
-  //   cy.get("#username").type('tester');
-  //   cy.get("#password").type('123123');
-  //   cy.get('[type="submit"]').click();
+  it("should show an error message when the password is incorrect", () => {
+    cy.get("#username").type('tester');
+    cy.get("#password").type('123123');
+    cy.get('[type="submit"]').click();
 
-  //   cy.get('.error').should('have.text', 'incorrect password');
-  // });
+    cy.get('.error').should('have.text', 'incorrect password');
+  });
 
   it("should proceed to the Dashboard Page", () => {
     cy.get("#username").type('tester');
