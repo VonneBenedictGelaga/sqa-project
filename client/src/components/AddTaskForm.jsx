af88e82c8178caa738a481cb3e9077de839f1625
iam-migz <mareyes2132@gmail.com>
2024-05-23 17:02:23 +0800

    update missing data tags, added codeowners

--- client/src/components/AddTaskForm.jsx
+++ client/src/components/AddTaskForm.jsx
@@ -16,12 +16,14 @@ function AddTaskForm({ saveTask }) {
       className="w-full px-4 pt-2 flex flex-col items-center sm:flex-row sm:items-end sm:justify-center sm:gap-4"
       onSubmit={onSubmit}
       autoComplete="off"
+      data-cy-root
     >
       <label className="form-control w-full sm:w-32">
         <div className="label">
           <span className="label-text">Title</span>
         </div>
         <input
+          data-cy="title-input"
           type="text"
           placeholder="title"
           className="input input-bordered input-sm"
@@ -37,6 +39,7 @@ function AddTaskForm({ saveTask }) {
           <span className="label-text">Description</span>
         </div>
         <input
+          data-cy="description-input"
           type="text"
           placeholder="description"
           className="input input-bordered input-sm"
@@ -48,6 +51,7 @@ function AddTaskForm({ saveTask }) {
       </label>
 
       <button
+        data-cy="add-button"
         className="btn btn-accent btn-sm w-full mt-4 sm:w-32"
         type="submit"
       >
